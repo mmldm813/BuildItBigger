fc3f36a3236c57dc728ed2ada69891b1ebbeb712
michellemedina <michelle200med@gmail.com>
2019-01-06 10:56:10 -0500

    Cleaned up errors

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -2,7 +2,6 @@ package com.udacity.gradle.builditbigger;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
--- backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyBean.java
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyBean.java
@@ -4,11 +4,8 @@ import com.example.jokeslibrary.Joker;
 
 /** The object model for the data we are sending through endpoints */
 public class MyBean {
-
-    private String myData;
-
     public String getData() {
-        myData = new Joker().getJoke();
+        String myData = new Joker().getJoke();
         return myData;
     }
 }
